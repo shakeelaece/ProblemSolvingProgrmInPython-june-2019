f189bc083ec5c4f2c16836d36309c3721a1a96bb
shakeelaece <51403262+shakeelaece@users.noreply.github.com>
2019-07-04 18:08:57 +0530

    Packages

---
+++ validators.py
@@ -0,0 +1,15 @@
+# Function to validate a phone number and email address
+
+import re
+
+def phoneNumberValidator(number):
+    pattern='^[6-9][0-9]{9}$|^[0][6-9][0-9]{9}|^[+][9][1][6-9][0-9]{9}$'
+    if re.match(pattern,str(number)):
+        return True
+    return False
+
+def emailValidator(email):
+    pattern='^[0-9a-z][0-9a-z_.]{4,13}[0-9a-z][@][0-9a-z]{3,18}[.][a-z]{2,4}$'
+    if re.match(pattern,email):
+        return True
+    return False
